f6ea5ef574a6c8718de3fcbbc162d919f4e32664
ulascankutay <ulascankutay@gmail.com>
2024-04-27 15:07:16 +0300

    gmail

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from machine import ADC,Pin
 from time import sleep
+from robust import MQTTClient
 import dht
 import network
-from robust import MQTTClient
 import gc
 import sys
+import umail
+import ntptime
+import time
+
 
 adc = ADC(0)
 sensor = dht.DHT11(Pin(16))
@@ -14,6 +18,17 @@ relay2 = Pin(4, Pin.OUT)
 
 relay1.value(1)
 relay2.value(1)
+
+
+config =      {'host'       : 'smtp.gmail.com',
+               'port'       : 587,
+               'username'   : 'nameulas@gmail.com',
+               'password'   : 'dgfdfg',
+               'from_name'  : 'ESP8266',
+               'to_name'    : 'Ulas Can',
+               'to'         : 'ulascankutay@gmail.com',
+               'subject'    : 'Nem Orani Bilgileendirme',
+               'text'       : 'nem oranı %10 altında'}
 
 #=======================================================
 THINGSPEAK_MQTT_CLIENT_ID = b"PSwCHCQEFSUhNBs6LB4OIBo"
@@ -35,6 +50,18 @@ try:
 except Exception as e:
     print('could not connect to MQTT server {}{}'.format(type(e).__name__, e))
     sys.exit()
+
+
+def  send_mail(congif):
+    print("mail gönderiliyor..")
+    smtp = umail.SMTP(config['host'], config['port'], username=config['username'], password=config['password'])
+    smtp.to(config['to'])
+    smtp.write("From: {0} <{0}>\n".format(config['from_name'], config['username']))
+    smtp.write("To: {0} <{0}>\n".format(config['to_name'], config['to']))
+    smtp.write("Subject: {0}\n".format(config['subject']))
+    smtp.send(config['text'])
+    smtp.quit()
+    print("mail gönderildi")
 
 
 def dht_sensor():
@@ -64,31 +91,40 @@ def toprak_sensor():
 def water_pump():
     
     deger, yüzde = toprak_sensor()
-    if yüzde<10:
+    sıcaklık, nem = dht_sensor()
+    if yüzde<10 or sıcaklık >50:
         buzzer.on()
-        relay1.value(0)   
-        sleep(1)
+        relay1.value(0)
+        sleep(5)
     else :
         buzzer.off()
         relay1.value(1)
         sleep(1)
     return relay1.value()
 
-
+def clock_set():
+    ntptime.settime()
+    saat = time.localtime()
+    localclk=str(saat[3]+3)+"0"+str(saat[4])
+    return int(localclk)
+    
 PUBLISH_PERIOD_IN_SEC = 15
 while True:
     sıcaklık, nem = dht_sensor()
     sleep(0.1)
     deger, yüzde = toprak_sensor()
     sleep(0.1)
+    if yüzde<10:
+        send_mail(config)   
     durum = water_pump()
-    
+    sleep(0.1)
+    saat=clock_set()
 
     
     try:
         freeHeapInBytes = gc.mem_free()
         credentials = bytes("channels/{:s}/publish".format(THINGSPEAK_CHANNEL_ID), 'utf-8')  
-        payload = bytes("field1={:.1f}&field2={:.1f}&field3={:.1f}&field4={:.1f}\n".format(sıcaklık, nem, yüzde,durum), 'utf-8')
+        payload = bytes("field1={:.1f}&field2={:.1f}&field3={:.1f}&field4={:.1f}&field5={:.1f}\n".format(sıcaklık, nem, yüzde,durum,saat), 'utf-8')
         client.publish(credentials, payload)
         sleep(PUBLISH_PERIOD_IN_SEC)
     except KeyboardInterrupt:
